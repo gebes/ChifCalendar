a4f308220c65d28ee82fa3eb5979685f235a4db7
Christoph Krassnigg <mail@gebes.eu>
2021-06-04 10:24:20 +0200

    :star: Feature enhancement

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.16
 require (
 	github.com/andersfylling/snowflake v1.3.0
 	github.com/apognu/gocal v0.8.0
+	github.com/bradfitz/slice v0.0.0-20180809154707-2b758aa73013
 	github.com/channelmeter/iso8601duration v0.0.0-20150204201828-8da3af7a2a61 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/nickname32/discordhook v1.0.2
 	github.com/robfig/cron v1.2.0
+	go4.org v0.0.0-20201209231011-d4a079459e60 // indirect
 )
--- webhook/calendar_cronjob.go
+++ webhook/calendar_cronjob.go
@@ -1,26 +1,54 @@
 package webhook
 
 import (
+	"fmt"
 	"gebes.io/calendar/utils"
 	"github.com/apognu/gocal"
 	"github.com/nickname32/discordhook"
 	"github.com/robfig/cron"
 	"log"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
 
+type Category struct {
+	title string
+	emoji string
+}
+
 const (
 	HomeworkEmoji = "📘"
 	ExamEmoji     = "📝"
-	Other         = "👻"
+	OtherEmoji    = "👻"
+
+	MessageTime = 17
+)
+
+var (
+	Categories = []Category{
+		{title: "Hausübungen", emoji: HomeworkEmoji},
+		{title: "Prüfungen", emoji: ExamEmoji},
+		{title: "Anderes", emoji: OtherEmoji},
+	}
+	ImportantKeywords = []string{"SA", "Schularbeit", "Test", "Revision"}
+	shortDayNames     = []string{
+		"So",
+		"Mo",
+		"Di",
+		"Mi",
+		"Do",
+		"Fr",
+		"Sa",
+	}
 )
 
 func InitCronJob() {
 	log.Println("Initializing CronJob")
 	c := cron.New()
-	c.AddFunc("0 17 * * *", sendEventsMessage)
+	c.AddFunc("0 0 "+strconv.Itoa(MessageTime)+" * * *", sendEventsMessage)
 	c.Start()
 	log.Println("Initialized CronJob")
 	sendEventsMessage()
@@ -28,88 +56,103 @@ func InitCronJob() {
 }
 
 func sendEventsMessage() {
-	exams, homework, other := "", "", ""
-	for _, event := range fetchTomorrowEvents() {
-		if strings.Contains(event.Summary, HomeworkEmoji) {
-			event.Summary = strings.TrimPrefix(event.Summary, HomeworkEmoji)
-			addRow(&homework, event)
-		} else if strings.Contains(event.Summary, ExamEmoji) {
-			event.Summary = strings.TrimPrefix(event.Summary, ExamEmoji)
-			addRow(&exams, event)
-		} else {
-			addRow(&other, event)
-		}
-	}
 
-	if len(exams) == 0 && len(homework) == 0 && len(other) == 0 {
+	tomorrowEmbed := getEmbedFor("Was steht morgen an?", fetchTomorrowEvents(), false)
+	sendEmbed(tomorrowEmbed)
+
+	nextWeekEmbed := getEmbedFor("Was steht in den nächsten Tagen an?", fetchNextWeekEvents(), true)
+	sendEmbed(nextWeekEmbed)
+
+}
+
+func sendEmbed(embed *discordhook.Embed) {
+	if embed == nil {
 		return
 	}
-
-	important := strings.Contains(strings.ToLower(exams), "schularbeit")
-
-	embedFields := []*discordhook.EmbedField{}
-
-	if len(exams) != 0 {
-		embedFields = append(embedFields, &discordhook.EmbedField{
-			Name:   ExamEmoji + " Prüfungen",
-			Value:  exams,
-			Inline: false,
-		})
-	}
-
-	if len(homework) != 0 {
-		embedFields = append(embedFields, &discordhook.EmbedField{
-			Name:   HomeworkEmoji + " Hausübungen",
-			Value:  homework,
-			Inline: false,
-		})
-	}
-	if len(other) != 0 {
-		embedFields = append(embedFields, &discordhook.EmbedField{
-			Name:   Other + " Anderes",
-			Value:  other,
-			Inline: false,
-		})
-	}
-
-	content := &discordhook.WebhookExecuteParams{
+	_, err := WA.Execute(nil, &discordhook.WebhookExecuteParams{
 		Embeds: []*discordhook.Embed{
-			{
-				Title:  "Was steht morgen an?",
-				Fields: embedFields,
-				URL:    "https://calendar.google.com/calendar/u/0?cid=aGEydTh1Z3Z2bDFrMWVucGhoM3M0cTE4bTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-				Footer: &discordhook.EmbedFooter{
-					IconURL: "https://i.imgur.com/meY76jE.png",
-					Text: "Calendar",
-				},
-
-			},
+			embed,
 		},
-	}
-
-	if important {
-		content.Content = "@everyone"
-		content.Embeds[0].Color = 0xff0000
-	}
-
-	msg, err := WA.Execute(nil, content, nil, "")
+	}, nil, "")
 	if err != nil {
 		panic(err)
 	}
-	log.Println(msg.ID)
 }
 
-func addRow(message *string, toAppend gocal.Event) {
-	*message += "• "+ toAppend.Summary
-	if len(toAppend.Description) != 0 {
-		*message += " **–** " + toAppend.Description
+func getEmbedFor(title string, events *[]gocal.Event, showDates bool) *discordhook.Embed {
+	messages := make([]string, len(Categories))
+	important := false
+
+	embedFields := []*discordhook.EmbedField{}
+	for i, category := range Categories {
+
+		for _, event := range *events {
+			if strings.Index(event.Summary, category.emoji) != 0 {
+				continue
+			}
+
+			addRow(&category, &messages[i], showDates, &event)
+		}
+
+		if !important && containsImportantKeyword(&messages[i]) {
+			important = true
+		}
+
+		if len(messages[i]) == 0 {
+			continue
+		}
+
+		embedFields = append(embedFields, &discordhook.EmbedField{
+			Name:   category.emoji + " " + category.title,
+			Value:  messages[i],
+			Inline: false,
+		})
+
+	}
+
+	if len(embedFields) == 0 {
+		return nil
+	}
+
+	embed := &discordhook.Embed{
+		Title:  title,
+		Fields: embedFields,
+		URL:    "https://calendar.google.com/calendar/u/0?cid=aGEydTh1Z3Z2bDFrMWVucGhoM3M0cTE4bTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+	}
+
+	if important {
+		embed.Color = 0xf2c94c
+	}
+
+	return embed
+
+}
+
+func addRow(category *Category, message *string, showDate bool, eventToAppend *gocal.Event) {
+	*message += "• "
+	if showDate {
+		year, month, day := eventToAppend.Start.Date()
+		dayOfWeek := eventToAppend.Start.Weekday()
+		*message += fmt.Sprintf("**%s. %02d.%02d.%04d** ", shortDayNames[dayOfWeek], day, month, year)
+	}
+
+	*message += strings.TrimPrefix(eventToAppend.Summary, category.emoji)
+	if len(eventToAppend.Description) != 0 {
+		*message += " **–** " + eventToAppend.Description
 	}
 	*message += "\n"
 }
 
-func fetchTomorrowEvents() []gocal.Event {
-	start, end := time.Now(), time.Now().Add(24*time.Hour)
+func fetchTomorrowEvents() *[]gocal.Event {
+	start, end := tomorrow(), tomorrow().Add(24*time.Hour)
+	return fetchEventsInPeriod(&start, &end)
+}
+func fetchNextWeekEvents() *[]gocal.Event {
+	start, end := tomorrow().Add(48*time.Hour), tomorrow().Add(8*24*time.Hour)
+	return fetchEventsInPeriod(&start, &end)
+}
 
+func fetchEventsInPeriod(start *time.Time, end *time.Time) *[]gocal.Event {
 	resp, err := http.Get(utils.GetCalendarKeys().CalenderURL)
 	if err != nil {
 		panic(err)
@@ -117,11 +160,30 @@ func fetchTomorrowEvents() []gocal.Event {
 
 	c := gocal.NewParser(resp.Body)
 
-	c.Start, c.End = &start, &end
+	c.Start, c.End = start, end
 	err = c.Parse()
 	if err != nil {
 		panic(err)
 	}
 
-	return c.Events
+	events := c.Events
+	sort.Slice(events[:], func(i, j int) bool {
+		return events[i].Start.Unix() < events[j].Start.Unix()
+	})
+	return &events
+}
+
+func tomorrow() time.Time {
+	loc, _ := time.LoadLocation("Europe/Vienna")
+	year, month, day := time.Now().AddDate(0, 0, 1).Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, loc)
+}
+
+func containsImportantKeyword(toCheck *string) bool {
+	for _, keyword := range ImportantKeywords {
+		if strings.Contains(*toCheck, keyword) {
+			return true
+		}
+	}
+	return false
 }
